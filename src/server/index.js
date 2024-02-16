a417878daefdd721ba612c8d6e2c53bf3b9d0fa3
Haider Ali <rmha64@gmail.com>
2024-02-16 22:48:00 +0500

    Updated server start log

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,12 @@
 import "dotenv/config.js";
 import "./utils/ensureEnv.js";
 import "./utils/cronJobs.js";
+import chalk from "chalk";
 import ViteExpress from "vite-express";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3001;
 
 ViteExpress.listen(app, PORT, () =>
-  console.log("server listening on port " + PORT)
+  console.log("server started: http://localhost:" + PORT)
 );
